a4872c9715d6d246b02208a8e94055d7dba6e1bf
Krizel Minnema <krizelminnema@gmail.com>
2019-10-26 18:45:40 -0700

    fixed body of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,32 +12,43 @@ document.onkeyup = function(event) {
 	var userGuess = event.key;
 
 // This is how the computer's guess is randomized and "thought of" for the user to guess.
+	var computerChoices = ("abcdefghijklmnopqrstuvwxyz").split("");
 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 // The options that the user and computer could choose from.
 	var options = ("abcdefghijklmnopqrstuvwxyz").split("");
-	var computerChoices = ("abcdefghijklmnopqrstuvwxyz").split("");
-
+	
 // The following are conditional statements to log the wins.
 	if (options.indexOf(userGuess) > -1) {
-
 		if (userGuess === computerGuess) {
 			wins++;
-			numGuesses = 9;
+			guessesLeft = 9;
 			guessChoices = [];
 		}
 
-// Everytime the user guesses soemthing that doesn't match the computer's guess, the decrementer will lower the number of guesses left. This is also where the guesses that the user has made hare pushed.
+// Everytime the user guesses soemthing that doesn't match the computer's guess, the decrementer will lower the number of guesses left. 
 		if (userGuess != computerGuess) {
-			numGuesses --;
-			guessChoices.push(userGuess);
-			guessChoices = [];
+			guessesLeft --;
+			guessChoices.push(userGuess);	
 		}
 
-// If there are 0 guesses left and they didn't get the letter, the loss counter increases. 
-		if (numGuesses === 0) {
-			numGuesses = 9;
+// If there are 0 guesses left and they didn't get the letter, the loss counter increases. This is also where the guesses that the user has made hare pushed.
+		if (guessesLeft === 0) {
+			guessesLeft = 9;
 			losses ++;
-	}	
+			guessChoices = [];
+		}	
+
+// This is the body of the game and where the wins, losses, guesses left and guessed letters are written.
+		var gameBody = 
+		"<h1> The Psychic Game </h1>" +
+		"<p>Guess what letter I'm thinking of:</p>" +
+		"<p>Wins: " + wins + "</p>" +
+		"<p>Losses: " + losses + "</p>" +
+		"<p>Guesses Left: " + guessesLeft + "</p>" +
+		"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+
+		document.querySelector("#game").innerHTML = gameBody;
+
 	}
 };
